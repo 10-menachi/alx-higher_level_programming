c70d2cc22fe52ca446bc951e14ea3b60a407b488
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-12-01 19:50:04 +0300

    3-infinite_add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
-    sum_of_args = 0
-    for i in range(1, len(sys.argv)):
-            sum_of_args += int(sys.argv[i])
-    print(sum_of_args)
+    from sys import argv
+
+    argc = len(argv)
+    if argc == 1:
+        add = 0
+    add = 0
+    index = 1
+    while index < argc:
+        add += int(argv[index])
+        index += 1
+    print(add)
