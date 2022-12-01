761b0404e8e10f88afd98f5787bda57742f23732
chrisdroid <bobitlmr188221@spu.ac.ke>
2022-12-01 13:39:07 +0300

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,10 @@
 if __name__ == '__main__':
     import sys
     length = len(sys.argv)
-    if length == 0:
+    if length == 1:
         print("{} arguments.".format(length - 1))
+    else if length == 2:
+        print("{} arguments:".format(length - 1))
     else:
         print("{} arguments:".format(length - 1))
 
