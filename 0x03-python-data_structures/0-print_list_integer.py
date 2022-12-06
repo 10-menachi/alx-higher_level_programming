8bbce22919c5e42602bc1eb03ed99084b9602fb7
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-06 12:11:48 +0300

    0-print_list_integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,3 @@ if __name__ == "__main__":
     def print_list_integer(my_list=[]):
         for item in my_list:
             print("{:d}".format(item))
-
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
