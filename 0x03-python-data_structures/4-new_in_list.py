ae660b8b3a7cba575348d458af5c70830b3ac89c
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-06 14:13:23 +0300

    4-new_in_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list2 = my_list
+    list2 = my_list.copy()
     if idx > len(list2) - 1 or idx < 0:
         return list2
     list2[idx] = element
