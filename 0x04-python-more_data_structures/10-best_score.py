8cb3efa50a99281e95ba05ff1623efbbe425a515
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-09 16:14:41 +0300

    10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    maxval = 0
-    maxkey = None
-    for key, value in a_dictionary.items():
-        if value > maxval:
-            maxval = value
-            maxval = key
-    return maxkey
+    """returns a key with the biggest integer value."""
+    if not a_dictionary:
+        return "None"
+    return max(a_dictionary)
