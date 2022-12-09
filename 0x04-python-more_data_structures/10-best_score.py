b95129bef948371e07124de97f7c35768f926aaa
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-09 16:12:31 +0300

    10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or a_dictionary is dict:
+    if a_dictionary is None:
         return None
-    init = list(a_dictionary.keys())[0]
-    maxim = a_dictionary[init]
+    maxval = 0
+    maxkey = None
     for key, value in a_dictionary.items():
-        if value > maxim:
-            maxim = value
-            init = key
-    return init
+        if value > maxval:
+            maxval = value
+            maxval = key
+    return maxkey
