7a0878670e8e0eb588a9b662444e1cb56ede19f2
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-22 20:55:02 +0300

    8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def islower(string):
+def uppercase(string):
     for char in string:
         print('{}'.format(chr(ord(char) - 32)), end='\n' if char == '\n' else '')
     print('')
