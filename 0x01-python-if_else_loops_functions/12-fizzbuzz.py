721e8e6fe5092342df0d325be0d1566964285c92
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-23 15:19:45 +0300

    12-fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,10 @@
-#!/usr/bin/python3
 def fizzbuzz():
-    for number in range(1, 101):
-        if number % 3 == 0 and number % 5 == 0:
-            print('FizzBuzz', end=', ')
-        elif number % 3 == 0:
-            print('Fizz', end=', ')
-        elif number % 5 == 0:
-            print('Buzz', end='' if number == 100 else ', ')
-        else:
-            print('{:d}'.format(number), end=', ')
-fizzbuzz()
+  for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+      print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+      print("Fizz", end=" ")
+    elif i % 5 == 0:
+      print("Buzz", end=" ")
+    else:
+      print(i, end=" ")
