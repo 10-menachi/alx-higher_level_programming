68744e4c8119fd5989101930ed9fc6243001a609
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-23 19:45:26 +0300

    5-no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_string = ""
     for char in my_string:
-        print(char)
-print(no_c('chris'))
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
