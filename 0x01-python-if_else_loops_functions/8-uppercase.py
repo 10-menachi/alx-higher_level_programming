54b0ac4e42f91e9545cf6b5b5b7ea5c374925508
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-23 11:26:40 +0300

    8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def uppercase(string):
+    result = ''
     for char in string:
-        print('{}'.format(chr(ord(char) - 32)), end='\n' if char == '\n' else '')
-    print('')
+        if ord(char) >= 97 and ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
