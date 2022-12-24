92098ec75be7bc7efd99076cf678ace247960be9
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-24 10:34:38 +0300

    13-is_palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,32 +1,49 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly
- * linked list is a palindrome
- *
- * @head: head of yhe list
- *
- * Return: 0 if not palindrome,
- * 1 if palindrome
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
  */
 
-int is_palindrome(listint_t **head)
+int _strlen_recursion(char *s)
 {
-	listint_t *current = *head;
-	int count = 0;
-	int values[1024];
+if (!*s)
+{
+return (0);
+}
+return (1 + _strlen_recursion(++s));
+}
 
-	while (current != NULL)
-	{
-		values[count] = current->n;
-		current = current->next;
-		count++;
-	}
+/**
+ * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
+ * Return: boolena
+ */
 
-	for (int i = 0; i < count / 2; i++)
-	{
-		if (values[i] != values[count - i - 1])
-			return (0);
-	}
-	return (1);
+int p1(char *s, int l)
+{
+if (l < 1)
+{
+return (1);
+}
+if (*s == *(s + l))
+{
+return (p1(s + 1, l - 2));
+}
+return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+int len = _strlen_recursion(s);
+
+return (p1(s, len - 1));
 }
