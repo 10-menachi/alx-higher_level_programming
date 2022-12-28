cc6290d3e4a5989377061b151df48965ad77350a
10-menachi <bobitlmr188221@spu.ac.ke>
2022-12-28 20:30:35 +0300

    2-safe_print_list_integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,11 @@
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for item in my_list:
-            if isinstance(item, (str, list)):
-                my_list.remove(item)
-        new_list = my_list[:x]
-        count = 0
-        for item in new_list:
-            print("{:d}".format(item), end='')
-            count += 1
-        print("")
+    count = 0
+    for i in range(x):
+        try:
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end=' ')
+                count += 1
+        except IndexError:
+            break
+        print()
         return count
-    except IndexError:
-        for item in my_list:
-            print("{:d}".format(item), end='')
