c9d292463873aaed3c09e1daf250002bb9746652
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-15 00:16:13 +0300

    Fix bug in code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
+        if idx < 0 or idx > len(my_list):
+            return my_list
         new_list = my_list
-        for i in range(0, len(new_list)):
-            pass
-        while i >= 0:
-            print("{:d}".format(new_list[i]))
-            i -= 1
+        new_list[idx] = element
+        return new_list
