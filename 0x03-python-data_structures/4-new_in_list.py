d408a3345e4daf500c7d2b0fcdb458f2e8a96ced
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-15 00:14:08 +0300

    Implement new_in_list function

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list:
+        new_list = my_list
+        for i in range(0, len(new_list)):
+            pass
+        while i >= 0:
+            print("{:d}".format(new_list[i]))
+            i -= 1
