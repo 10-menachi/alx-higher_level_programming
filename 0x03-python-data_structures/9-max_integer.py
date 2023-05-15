30e5d80c9e74dad734db7846d978f49c72f7d78a
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-15 17:21:14 +0300

    Fix bug in code

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     maximum = my_list[0]
-    if len(my_list) > 0:
+    if my_list:
         for elem in my_list:
             if elem > maximum:
                 maximum = elem
