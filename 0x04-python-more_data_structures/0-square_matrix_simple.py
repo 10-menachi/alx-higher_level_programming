4792267cf513cca7522ea1b4aa14e33b5085bf58
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-18 09:08:19 +0300

    Fix bug in code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    copy_matrix = matrix.copy()
+    copy_matrix = [[0] * len(matrix) for row in matrix]
     for i in range(len(copy_matrix)):
         for j in range(len(copy_matrix[i])):
-            copy_matrix[i][j] = copy_matrix[i][j] ** 2
+            copy_matrix[i][j] = matrix[i][j] ** 2
     return copy_matrix
