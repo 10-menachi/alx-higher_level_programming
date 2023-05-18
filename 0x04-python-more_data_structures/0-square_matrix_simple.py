f7c9254e046ad10221bfb050b2ed7d974f9e9837
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-18 09:00:36 +0300

    Implement square_matrix_simple function

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    copy_matrix = matrix.copy()
+    for i in range(len(copy_matrix)):
+        for j in range(len(copy_matrix[i])):
+            copy_matrix[i][j] = copy_matrix[i][j] ** 2
+    return copy_matrix
