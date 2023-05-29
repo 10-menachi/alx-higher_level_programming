573fbc319f51afb6ed53cd60eb13913a5ddce447
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-29 13:40:56 +0300

    Fix bug in code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,17 @@
 def safe_print_list(my_list=[], x=0):
     for index in range(x):
         try:
-            print("{:d}".format(my_list[index]))
+            print("{:d}".format(my_list[index]), end="")
         except IndexError:
             pass
     return x
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
