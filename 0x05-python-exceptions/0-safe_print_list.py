877955de8c4af6d5fead8772ce6ca156a8231a6b
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-29 09:39:23 +0300

    Implement safe_print_integer function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     for index in range(x):
         try:
-            print(my_list[index])
+            print("{:d}".format(my_list[index]))
         except IndexError:
             pass
     return x
