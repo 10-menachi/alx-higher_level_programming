083aaf148583f5c446ce4d8febac5737c43b789e
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-30 16:37:15 +0300

    Fix linter error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for index in range(0, x):
         try:
-            print("{:d}".format(my_list[index]), end="")
             count += 1
-        except:
-            pass
+            print("{:d}".format(my_list[index]), end="")
+        except IndexError:
+            break
     print()
     return x
