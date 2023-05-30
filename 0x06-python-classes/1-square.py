aac17b47c5c17f175853b9881ba7fadd8b49f33a
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-30 18:01:12 +0300

    Create a class called Square with a size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines a class called Square with an attribute named size"""
+
+
+class Square:
+    """ Defines a class called Square with an attribute named size"""
+
+    def __init__(self, size) -> None:
+        self._size = size
