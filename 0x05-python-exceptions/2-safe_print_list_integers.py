70cd001a581d25230317b51c726633e713e4deda
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-05-30 16:47:50 +0300

    Fix bug in code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,6 +4,20 @@ def safe_print_list_integers(my_list=[], x=0):
     for index in range(0, x):
         try:
             count += 1
-            print("{:d}".format(my_list[index]))
+            print("{:d}".format(my_list[index]), end="")
         except (ValueError, TypeError):
             pass
+    return count
+
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
