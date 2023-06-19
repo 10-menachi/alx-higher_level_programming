0fa1402bb9fd0021e687f79c9525d929221d2d39
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2023-06-19 13:50:48 +0300

    Delete 0x06-python-classes directory

--- 0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-""" Defines an empty class called Square """
-
-
-class Square:
-    """ Defines an empty class called Square """
-    pass
--- 0x06-python-classes/1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Defines a class called Square with an attribute named size"""
-
-
-class Square:
-    """ Defines a class called Square with an attribute named size"""
-
-    def __init__(self, size) -> None:
-        self.__size = size
