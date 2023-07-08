0cf55dc1827a4a630ac5e52100f3eefd235dd56f
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-08 11:30:15 +0300

    Fix bug in code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,18 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0) -> None:
-        self.__width = width
-        self.__height = height
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif width < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = height
 
     def width(self, width):
         if type(width) != int:
