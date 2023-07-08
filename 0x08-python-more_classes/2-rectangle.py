9e241711fcf31453b6ea6df06bad1b7762910add
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-08 12:15:37 +0300

    Fix bug in code

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,11 +43,11 @@ class Rectangle:
             self.__height = value
 
     def area(self):
-        if self.__height or self.__width == 0:
+        if self.__height and self.__width == 0:
             return 0
         return self.__height * self.__width
 
     def perimeter(self):
-        if self.__height or self.__width == 0:
+        if self.__height and self.__width == 0:
             return 0
         return 2 * (self.__height + self.__width)
