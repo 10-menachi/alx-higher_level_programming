c6852100047d0bfc313dd7a88ad29e72c271da94
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-11 14:34:20 +0300

    Implement BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+This module contains a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """class with public instance method area()"""
+
+    def area(self):
+        """raises an Exception with the message area() is not implemented"""
+        raise Exception("area() is not implemented")
