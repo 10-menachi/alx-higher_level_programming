d74ea10db956ec20902d4c530f96cd35c335202b
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-12 13:34:30 +0300

    Fix bug in code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -16,11 +16,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def area(self):
-        """Returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns the rectangle description"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
