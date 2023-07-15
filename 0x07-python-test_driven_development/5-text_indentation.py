f19bf34bd86479c93b6a384843f4b948b59949cf
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-15 19:06:12 +0300

    Fix bug in code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,6 +11,7 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    text = text.strip()
     for char in text:
         print(char, end='')
         if char in ('.', '?', ':'):
