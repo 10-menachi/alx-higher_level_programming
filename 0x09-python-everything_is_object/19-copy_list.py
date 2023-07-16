1934b27c8d7af4452947e85a83e7d545d95f52ed
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 12:50:05 +0300

    Fix bug in code

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-
-"""
-This module contains a function that returns a copy of a list.
-"""
-
-
-def copy_list(l):
-    """
-    Returns a copy of a list.
-    """
-    return l[:]
+def copy_list(given_list):
+    return given_list[:]
