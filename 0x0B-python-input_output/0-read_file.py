575e8af336e8970c0d7fcf1e3eae77aab5b32af6
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 18:55:29 +0300

    Fix bug in code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This module contains a function that reads a text file (UTF8) and prints it to
 stdout.
@@ -9,4 +8,4 @@ stdout.
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
