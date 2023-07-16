9e86b5698ec6633ab1dd61b038d4027986249df3
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 13:06:14 +0300

    Document class and module

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""
+This module contains a class called LockedClass
+"""
+
+
 class LockedClass:
+    """
+    This class has no class or object attribute
+    """
     __slots__ = ['first_name']
 
     def __init__(self, first_name=""):
