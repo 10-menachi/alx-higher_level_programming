4912610861d0b4a0fd541a2382ca51192f8a2a08
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 10:39:41 +0300

    Fix bug in text_indentation function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,10 +12,7 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.strip()
-    result = ""
-    for char in text:
-        result += char
-        if char in ('.', '?', ':'):
-            result += '\n' * 2
-    print(result, end="")
+    for char in ".?:":
+        text = (char + "\n\n").join(
+            [line.strip(" ") for line in text.split(char)])
+        print("{}".format(text), end='')
