5319c209539e840ececa2b09956df038575fa9c1
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 11:01:36 +0300

    Implement lazy_matrix_mul function

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+This module contains a function that multiplies 2 matrices.
+"""
+
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Multiplies two matrices using NumPy.
+    """
+    validate_matrix(m_a, "matrix_a")
+    validate_matrix(m_b, "matrix_b")
+
+    np_a = np.array(m_a)
+    np_b = np.array(m_b)
+
+    if np_a.shape[1] != np_b.shape[0]:
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = np.matmul(np_a, np_b)
+
+    return result.tolist()
+
+
+def validate_matrix(matrix, name_of_matrix):
+    """
+    Validates the matrix for the required conditions.
+    """
+    if not isinstance(matrix, list):
+        raise TypeError(f"{name_of_matrix} must be a list")
+
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError(f"{name_of_matrix} must be a list of lists")
+
+    if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
+        raise ValueError(f"{name_of_matrix} can't be empty")
+
+    for row in matrix:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError(
+                    f"{name_of_matrix} should contain only integers or floats")
+
+    row_sizes = set(len(row) for row in matrix)
+    if len(row_sizes) > 1:
+        raise TypeError(
+            f"each row of {name_of_matrix} must be of the same size")
