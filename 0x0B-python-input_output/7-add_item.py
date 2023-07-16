5d4236de80d3b7082bab0b5d478de8fbff3516e0
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 19:23:14 +0300

    Reimplement add_item code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,19 +4,20 @@ This module contains a script that adds all arguments to a Python list,
 and then save them to a file.
 """
 
+import sys
+import os.path
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+text_file = "add_item.json"
+list_to_add = []
 
-def add_item(args):
-    """
-    This function adds all arguments to a Python list,
-    and then save them to a file.
-    """
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except:
-        my_list = []
-    for i in args:
-        my_list.append(i)
-    save_to_json_file(my_list, "add_item.json")
+if os.path.isfile(text_file):
+    list_to_add = load_from_json_file(text_file)
+
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        list_to_add.append(sys.argv[i])
+
+save_to_json_file(list_to_add, text_file)
