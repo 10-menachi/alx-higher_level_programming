e1886f9f388ab34b9b1ed5bb6463e9f2c6e34629
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 10:53:34 +0300

    Fix bug in matrix_mul and validate_matrix functions

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -9,8 +9,8 @@ def matrix_mul(m_a, m_b):
     """
     Multiplies two matrices.
     """
-    validate_matrix(m_a)
-    validate_matrix(m_b)
+    validate_matrix(m_a, "m_a")
+    validate_matrix(m_b, "m_b")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
@@ -28,24 +28,26 @@ def matrix_mul(m_a, m_b):
     return result
 
 
-def validate_matrix(matrix):
+def validate_matrix(matrix, name_of_matrix):
     """
     Validates the matrix for the required conditions.
     """
     if not isinstance(matrix, list):
-        raise TypeError("m_a must be a list")
+        raise TypeError(f"{name_of_matrix} must be a list")
 
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("m_a must be a list of lists")
+        raise TypeError(f"{name_of_matrix} must be a list of lists")
 
     if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
-        raise ValueError("m_a can't be empty")
+        raise ValueError(f"{name_of_matrix} can't be empty")
 
     for row in matrix:
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("m_a should contain only integers or floats")
+                raise TypeError(
+                    f"{name_of_matrix} should contain only integers or floats")
 
     row_sizes = set(len(row) for row in matrix)
     if len(row_sizes) > 1:
-        raise TypeError("each row of m_a must be of the same size")
+        raise TypeError(
+            f"each row of {name_of_matrix} must be of the same size")
