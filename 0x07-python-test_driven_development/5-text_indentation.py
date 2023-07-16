3271282d9eb19f070842b7ef0810ab5f3b645109
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-16 10:44:49 +0300

    Fix bug in text_indentation function

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,15 +7,19 @@ This module contains a function that prints a text with 2 new lines after each
 
 def text_indentation(text):
     """
-    This function prints a text
-    with 2 new lines after each of these characters:
+    This function prints a text with 2 new lines after each of these characters:
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     text = text.strip()
     result = ""
+    indentation = ""
     for char in text:
-        result += char
+        result += indentation + char
         if char in ('.', '?', ':'):
-            result += '\n' * 2
+            result += '\n\n'
+        if char == ' ' and indentation == '':
+            indentation = '    '
+        elif char != ' ' and indentation != '':
+            indentation = ''
     print(result, end="")
