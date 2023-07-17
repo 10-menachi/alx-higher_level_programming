d2605a14ce2cca22d19b553ea35d9d391f99e653
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-07-17 19:33:30 +0300

    Fix indentation and import statement

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ This module contains a class called Rectangle
 that inherits from the Base class
 """
 
-Base = __import__('base').Base
+from base import Base
 
 
 class Rectangle(Base):
@@ -23,84 +23,84 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-        @property
-        def width(self):
-            """
-            This is the getter method for the width attribute
-            """
-            return self.__width
+    @property
+    def width(self):
+        """
+        This is the getter method for the width attribute
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """
-            This is the setter method for the width attribute
-            """
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            elif value <= 0:
-                raise ValueError("width must be > 0")
-            else:
-                self.__width = value
+    @width.setter
+    def width(self, value):
+        """
+        This is the setter method for the width attribute
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
 
-        @property
-        def height(self):
-            """
-            This is the getter method for the height attribute
-            """
-            return self.__height
+    @property
+    def height(self):
+        """
+        This is the getter method for the height attribute
+        """
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """
-            This is the setter method for the height attribute
-            """
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value <= 0:
-                raise ValueError("height must be > 0")
-            else:
-                self.__height = value
+    @height.setter
+    def height(self, value):
+        """
+        This is the setter method for the height attribute
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
 
-        @property
-        def x(self):
-            """
-            This is the getter method for the x attribute
-            """
-            return self.__x
+    @property
+    def x(self):
+        """
+        This is the getter method for the x attribute
+        """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """
-            This is the setter method for the x attribute
-            """
-            if type(value) is not int:
-                raise TypeError("x must be an integer")
-            elif value < 0:
-                raise ValueError("x must be >= 0")
-            else:
-                self.__x = value
+    @x.setter
+    def x(self, value):
+        """
+        This is the setter method for the x attribute
+        """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
 
-        @property
-        def y(self):
-            """
-            This is the getter method for the y attribute
-            """
-            return self.__y
+    @property
+    def y(self):
+        """
+        This is the getter method for the y attribute
+        """
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """
-            This is the setter method for the y attribute
-            """
-            if type(value) is not int:
-                raise TypeError("y must be an integer")
-            elif value < 0:
-                raise ValueError("y must be >= 0")
-            else:
-                self.__y = value
+    @y.setter
+    def y(self, value):
+        """
+        This is the setter method for the y attribute
+        """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
 
-        def area(self):
-            """
-            This method returns the area of the Rectangle instance
-            """
-            return self.__width * self.__height
+    def area(self):
+        """
+        This method returns the area of the Rectangle instance
+        """
+        return self.__width * self.__height
