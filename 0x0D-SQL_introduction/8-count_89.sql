edb4de32ebbf9aa9e2718ad86ac6724b89bc2aa9
10-menachi <bobitlmr188221@spu.ac.ke>
2023-08-15 15:06:06 +0300

    Fix bug in code

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- This script retrieves rows with the id 89
-SELECT * FROM first_table WHERE id = 89;
+-- This script retrieves number of rows with the id 89
+SELECT COUNT(*) FROM `table` WHERE `id` = 89
