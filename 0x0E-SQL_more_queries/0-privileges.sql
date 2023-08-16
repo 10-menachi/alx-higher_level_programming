6631e10119e2bda0c2599a98636cb8376651b34f
10-menachi <bobitlmr188221@spu.ac.ke>
2023-08-16 16:24:39 +0300

    Fix bug in code

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- This script lists all privileges granted to given users.
-SHOW GRANTS FOR 'user_0d_1'@'localhost' IF EXISTS;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- List privileges for another user.
-SHOW GRANTS FOR 'user_0d_2'@'localhost' IF EXISTS;
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
