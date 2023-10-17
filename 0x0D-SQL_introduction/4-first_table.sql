b3137fab27e81620f6f2ea56fc2f6d1ff69b5788
10-menachi <bobitlmr188221@spu.ac.ke>
2023-10-18 00:35:46 +0300

    Fix Syntax Errors

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- This script creates the table first_table in the current database if it does not exist.
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
-    name VARCHAR(256),
+    name VARCHAR(256)
 );
