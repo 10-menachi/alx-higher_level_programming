b93e4aae44ff9d0cfd68465f436783f9695fa327
10-menachi <bobitlmr188221@spu.ac.ke>
2023-12-14 09:23:09 +0300

    Complete task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- This script creates a table second_table (database hbtn_0c_0) in your MySQL server and add multiples rows.
+CREATE TABLE second_table (id INT, name VARCHAR(256));
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
