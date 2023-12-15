83805aa9e163eb778ae6291666f5a9e431a9178f
10-menachi <bobitlmr188221@spu.ac.ke>
2023-12-15 07:05:04 +0300

    Fix bug in code

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,5 @@
 -- This script creates the table force_name on your MySQL server
 CREATE TABLE IF NOT EXISTS force_name(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
 );
