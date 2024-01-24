ca858dfbee894a273f49c700152e7d5180182dcf
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-01-24 12:34:08 +0300

    Fix bug in code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,12 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%'"
+    )
+    cursor.execute(
+        "COLLATE Latin1_General_CS_AS ORDER BY id ASC;"
+    )
     rows = cursor.fetchall()
     for row in rows:
         print(row)
