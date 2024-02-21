e9e23e033d2439ead3bd8ba23e1d7d8cff91ee54
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-02-21 16:51:02 +0300

    Fix bug in code

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,14 @@ const url = process.argv[2];
 
 request(url, function (error, response, body) {
   if (error) {
-    console.error('error:', error);
+    console.error(error);
   } else {
-    console.log(
-      JSON.parse(body).results.filter((element) => {
-        return (
-          element.characters.filter((character) => character.includes('18'))
-            .length > 0
-        );
-      })
-    );
+    const count = JSON.parse(body).results.filter((element) => {
+      return (
+        element.characters.filter((character) => character.includes('18'))
+          .length > 0
+      );
+    });
+    console.log(count.length);
   }
 });
