752477a1a09b51cfdef08afc3e1093022aa3af0b
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-02-21 16:37:38 +0300

    Fix bug in code

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/:id';
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
 request.get(url).on('response', function (response) {
   const title = JSON.parse(response.body).title;
