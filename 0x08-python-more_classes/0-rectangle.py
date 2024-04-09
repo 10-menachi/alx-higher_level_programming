17f2429b850a3cc17971d1337bf1764765c0478d
Christian Wamalwa <bobitlmr188221@spu.ac.ke>
2024-04-09 08:57:07 +0300

    Complete task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module defines a class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    This class defines a rectangle.
+    """
+    pass
