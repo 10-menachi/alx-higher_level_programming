3ae9a963536132db3d939cff8a8188af49a587fc
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-07-29 21:19:08 +0300

    Complete task 0, 1, 2 and 3

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""This module creates a class Square."""
+
+class Square:
+    """Square class with a private attribute size."""
+    def __init__(self, size=0):
+        """Initializes the size variable as a private instance attribute."""
+        self.__size = size
---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This module creates a class Square."""
+
+class Square:
+    """Square class with a private attribute size."""
+    def __init__(self, size=0):
+        """Initializes the size variable as a private instance attribute."""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.area = lambda: self.__size ** 2
+        self.perimeter = lambda: self.__size * 4
+        self.__str__ = lambda: f"[Square] {self.__size}/{self.__size}"
+        self.__repr__ = lambda: f"[Square] {self.__size}/{self.__size}"
+        self.__del__ = lambda: print("Bye rectangle...")
