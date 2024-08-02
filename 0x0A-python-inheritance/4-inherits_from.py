8ced4a8a9925a4e61fd324ebf24ae12421b543d7
chrisdroid <bobitlmr188221@spu.ac.ke>
2024-08-02 11:17:22 +0300

    Fix linting errors

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,14 +2,11 @@
 
 """
 This module contains a function that returns True if the object is an instance
-of a class that inherited (directly or indirectly) frocm the specified class,
+of a class that inherited (directly or indirectly) from the specified class,
 """
 
 
 def inherits_from(obj, a_class):
     """Returns True if the object is an instance of a class that inherited
     (directly or indirectly) from the specified class, otherwise False"""
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class) and type(obj) is not a_class
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
 
 """
-This module contains a BaseGeometry class
+This module contains a BaseGeometry class.
 """
 
 
 class BaseGeometry:
-    """class with public instance method area() and integer_validator()"""
+    """A class with public instance methods for geometric operations."""
 
     def area(self):
-        """raises an Exception with the message area() is not implemented"""
+        """Raises an Exception with the message 'area() is not implemented'."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates value"""
-        if type(value) != int:
+        """Validates that value is a positive integer.
+
+        Args:
+            name (str): The name of the variable (used for error messages).
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
